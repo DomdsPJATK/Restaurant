7f64b28be74af20be98cfcb01455b4a9e74a49a5
DomdsPJATK <61829714+DomdsPJATK@users.noreply.github.com>
2021-03-06 02:57:44 +0100

    Redundant has been deleted

--- src/components/Home/styles/Home.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const HomeContainer = styled.div`
-    width: 1300px;
-    grid-auto-flow: row;
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
-    grid-auto-rows: auto;
-`;
